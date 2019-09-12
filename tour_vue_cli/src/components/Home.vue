bc30c5c19cf0364e4b46c0d990815761c5250b56
moweifeng <957796031@qq.com>
2019-09-12 10:22:14 +0800

    tabbar

---
+++ tour_vue_cli/src/components/Home.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <mt-tab-container v-model="active">
+            <mt-tab-container-item id="index">
+                <span>林氏集团负责</span>
+            </mt-tab-container-item>
+            <mt-tab-container-item id="find">
+                <span>暂无</span>
+            </mt-tab-container-item>
+            <mt-tab-container-item id="strategy">
+                <span>栋栋负责</span>
+            </mt-tab-container-item>
+            <mt-tab-container-item id="distanceRun">
+                <span>大佬负责</span>
+            </mt-tab-container-item>
+            <mt-tab-container-item id="me">
+                <span>太子负责</span>
+            </mt-tab-container-item>
+        </mt-tab-container>
+
+        <mt-tabbar v-model="active" fixed>
+            <mt-tab-item id="index">
+                <img :src="active=='index'?require('../assets/img/ic_element_tabbar_home_pressed.png'):require('../assets/img/ic_element_tabbar_home_normal.png')" class="active-icon" slot="icon">
+                <span>首页</span>
+            </mt-tab-item>
+            <mt-tab-item id="find">
+                <img :src="active=='find'?require('../assets/img/ic_element_tabbar_discovery_pressed.png'):require('../assets/img/ic_element_tabbar_discovery_normal.png')" class="active-icon" slot="icon">
+                <span>发现</span>
+                </mt-tab-item>
+            <mt-tab-item id="strategy">
+                <img :src="active=='strategy'?require('../assets/img/ic_element_tabbar_destination_pressed.png'):require('../assets/img/ic_element_tabbar_destination_normal.png')" class="active-icon" slot="icon">
+                <span>攻略</span>
+                </mt-tab-item>
+            <mt-tab-item id="distanceRun">
+                <img :src="active=='distanceRun'?require('../assets/img/ic_element_tabbar_schedule_pressed.png'):require('../assets/img/ic_element_tabbar_schedule_normal.png')" class="active-icon" slot="icon">
+                <span>行程</span>
+                </mt-tab-item>
+            <mt-tab-item id="me">
+                <img :src="active=='me'?require('../assets/img/ic_element_tabbar_user_pressed.png'):require('../assets/img/ic_element_tabbar_user_normal.png')" class="active-icon" slot="icon">
+                <span>我的</span>
+                </mt-tab-item>
+        </mt-tabbar>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            active:"index",
+        }
+    },
+}
+</script>
+<style scoped>
+
+</style>
+<style>
+    .mint-tabbar>.mint-tab-item.is-selected{
+        color:#000;
+    } 
+</style>
--- tour_vue_cli/src/router.js
+++ tour_vue_cli/src/router.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from './components/Home.vue'
 import HelloContainer from "./components/HelloWorld.vue"
 Vue.use(Router)
 export default new Router({
   routes: [
+    {path:'/Home',component:Home},
+    {path:'/',component:HelloContainer},
   ]
 })
