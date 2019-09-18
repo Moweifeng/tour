6490c904492b4c3936ffeaf63f240c2ef0cb2452
luoadmin <2923568121a>
2019-09-18 19:56:05 +0800

    111

--- tour_vue_cli/src/components/user/User.vue
+++ tour_vue_cli/src/components/user/User.vue
@@ -11,37 +11,109 @@
             <span class="username">烬水楼台先得月</span>
         </div>
          <a href="#" class="kefu" style="color:black">
-                <img src="../../img/userimg/kefu.png">   我的客服</a>   
+                <img src="../../img/userimg/kefu.png"> 我的客服 </a>   
                 <div class="xinxi">
                     <ul>
-                        <li><p>0</p> 收藏</li>
-                        <li><p>0</p>浏览历史</li>
-                        <li><p>0</p>红包卡券</li>
-                        <li><p>0</p>购物车</li>
+                        <li><a href="#"><p>0</p> 收藏</a></li>
+                        <li><a href="#"><p>0</p>浏览历史</a></li>
+                        <li><a href="#"><p>0</p>红包卡券</a></li>
+                        <li><a href="#"><p>0</p>购物车</a></li>
                     </ul>
             </div>
             <div class="huiyuan">
-                 <span class="zhongxin">会员权益中心</span>
-                 <span class="jingyan">快速升级|可领取4500经验值，助你提前享受高等级会员特权</span>
+                <div class="huiyuan1">
+                 <span class="zhongxin">会员权益中心 </span>
+                 <span class="flypig"><a href="#">成为飞猪会员</a></span>
+                 </div>
+                 <span class="jingyan"><a href="#">快速升级|可领取4500经验值，助你提前享受高等级会员特权</a></span> 
+            </div>
+            <div class="userdingdan">
+             <div class="dingdan">
+                  <span class="userdd">我的订单</span>
+                  <span class="alldd"><a href="#">全部订单 ></a></span>
+             </div>
+             <div class="dingdanys">
+                  <ul>
+                      <li><a href="#"><img>待付款</a></li>
+                      <li><a href="#"><img>处理中</a></li>
+                      <li><a href="#"><img>待出行</a></li>
+                      <li><a href="#"><img>待评价</a></li>
+                      <li><a href="#"><img>退款/售后</a></li>
+                     
+                  </ul>
+             </div>
             </div>
         </div>
     </div>
 </template>
 <style scoped>
+.dingdanys ul li a{
+    color:#5d5858;
+    font-size: 15px;
+}
+.dingdanys ul li{
+    margin-top: 50px;
+    width: 20%;
+    text-align: center;
+}
+.userdd{
+    color:black;    
+    font-weight:bold;
+}   
+.alldd a{
+    color: #ccc;
+    font-size: 13px;
+}
+.dingdan{
+    margin: 10px 10px;
+    display: flex;
+    justify-content:space-between;
+
+}
+.userdingdan{
+    border: 1px solid #ccc;
+    width: 95%;
+    margin: 0 auto;
+    margin-top: 10px;
+    height: 125px;
+    border-radius: 12px;
+}
+.huiyuan1{
+    display: flex;
+    justify-content:space-between;
+}
+.flypig a{
+    color: white;
+    font-size: 10px;
+    padding: 0 5px;
+}
+.flypig{
+    border: 1px solid red;
+    border-radius: 50px;
+    margin-top: 14px;
+    background: #ff4848;
+    padding: 1px;
+    margin-right: 10px;
+    
+}
+.jingyan a{
+    color: white;
+    margin-left: 10px;
+}
 .huiyuan .jingyan{
     display: flex;
     color: white;
     border: 1px solid #ccc;
     background: #beb7b7a3;
     font-size: 10px;
-    margin-top: 42px;
+    margin-top: 32px;
     margin-left: 10px;
     width: 95%;
     border-radius: 50px;
 }
 .huiyuan .zhongxin{
     display: flex;
-    margin-top: 10px;
+    margin-top: 16px;
     margin-left: 10px;
     color: #ccc;
     font-size: 18px;
@@ -51,11 +123,9 @@
     width:95%;
     margin: 0px auto;
     border: 1px solid white;
-    border-radius:3%; 
+    border-radius:12px; 
     height: 100px;
     background: rgb(185, 131, 86)
-}
-.huiyuan span{
 }
 .xinxi ul li{
    width:25%;
@@ -69,6 +139,7 @@ ul{
 }
 a{
     text-decoration: none;
+    color: black;
 }
     .touxiang{
        width: 22%;
