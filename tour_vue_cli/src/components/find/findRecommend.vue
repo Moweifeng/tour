de2ca6269e162a14432f269065cce0e8745c9896
moweifeng <957796031@qq.com>
2019-09-20 20:57:22 +0800

    add sql

---
+++ tour_vue_cli/src/components/find/findRecommend.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+        <div class="find-video">
+            <video src="../../img/find/x.mp4"></video>
+        </div>
+        <div class="find-content">
+            <div class="content-left">
+                <div class="find-loaction">
+                    广州
+                </div>
+                <div></div>
+            </div>
+            <div class="content-right"></div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style scoped>
+    .find-video video{
+        width: 100%; 
+        margin-top:200px; 
+    }
+
+</style>
